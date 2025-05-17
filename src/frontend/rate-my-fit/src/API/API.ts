8052512d265a78c40eef630bdee7ec57b392ce7e
Nick <nickorovkinwork@gmail.com>
2025-05-17 18:31:45 +0300

    build: Put API_URL in .env file for frontend + remove static and pfp files

--- src/frontend/rate-my-fit/src/API/API.ts
+++ src/frontend/rate-my-fit/src/API/API.ts
@@ -1,6 +1,6 @@
 import { RequestParams } from '../types/API';
 
-export const API_URL = 'http://localhost:8080';
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export const APICall = async (requestParams: RequestParams) => {
     const response = await fetch(API_URL + requestParams.path, {
