a710ccf93527a16a7044bbf1c7b29ab3b7eac9b9
Nick <nickorovkinwork@gmail.com>
2025-06-26 18:21:36 +0300

    refactor: Remove unused utils functions

--- src/backend/utils.py
+++ src/backend/utils.py
@@ -30,43 +30,6 @@ def collect_pics(pics: list[UploadFile], pic_status: dict) -> list[str]:
     return picnames
 
 
-def get_twin_id(user_id: str) -> str:
-    if user_id.startswith("@"):
-        twin_id = "userToken"
-        user_data = Database.Users.find_one({"username": user_id[1:]})
-    else:
-        twin_id = "username"
-        user_data = Database.Users.find_one({"userToken": user_id})
-
-    if user_data is None:
-        raise HTTPException(status_code=404, detail="No such user")
-
-    return user_data[twin_id]
-
-
-def find_by_relation(
-    collection, condition: dict, hide: dict, start: int = None, limit: int = None
-) -> list:
-    result_start = start if start is not None else 0
-
-    query_params = {}
-    if limit is not None:
-        query_params["skip"] = start
-        query_params["limit"] = result_start + limit
-
-    results_cursor = collection.find(condition, hide, **query_params)
-    return list(results_cursor)
-
-
-def check_items(items_id: list[str]) -> bool:
-    check = True
-    for item_id in items_id:
-        if Database.Items.find_one({"itemID": item_id}) is None:
-            check = False
-
-    return check
-
-
 def find_pfp(user_token: str) -> str:
     pfp_files = os.listdir("pfp")
     for filename in pfp_files:
@@ -74,32 +37,3 @@ def find_pfp(user_token: str) -> str:
             return filename
 
     raise HTTPException(status_code=404, detail="No pfp for this user")
-
-
-def get_user_stats(user_id: str) -> dict:
-    if user_id.startswith("@"):
-        user_token = get_twin_id(user_id)
-    else:
-        user_token = user_id
-
-    user_fits = list(Database.Fits.find({"authorToken": user_token}))
-    user_reviews = list(Database.Reviews.find({"authorToken": user_token}))
-
-    return {"fits": len(user_fits), "reviews": len(user_reviews)}
-
-
-def get_fit_stats(fit_data: dict) -> dict:
-    total_price = 0
-    for item_id in fit_data["itemsID"]:
-        total_price += Database.Items.find_one({"itemID": item_id})["price"]
-
-    fit_reviews = list(Database.Reviews.find({"fitID": fit_data["fitID"]}))
-    total_reviews = len(fit_reviews)
-    grades_sum = sum(review["grade"] for review in fit_reviews)
-    avg_grade = grades_sum / total_reviews if total_reviews else 0
-
-    return {
-        "totalPrice": total_price,
-        "totalReviews": total_reviews,
-        "avgGrade": avg_grade,
-    }
